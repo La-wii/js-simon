c913911c2ac68c4406e066d4b40126bb524fb2fe
La-wii <sentilavale@gmail.com>
2021-06-11 16:34:16 +0200

    corretto ciclo for numeri random

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var numRandom;
 var numbers = [];
 
-for (var i = 0; i <= 5 ; i++){
+for (var i = 0; i < 5 ; i++){
 
     numRandom = getRndInteger(1, 100);
 
@@ -14,14 +14,36 @@ for (var i = 0; i <= 5 ; i++){
     }
 }
 
+console.log(numbers);
 // Un alert() espone 5 numeri generati casualmente.
 var numPerUtente = alert('Prova ricordare questi numeri:' + ' ' + numbers); 
 
 
 // Da li parte un timer di 30 secondi.
+// test con 5 secondi per velocizzare i tempi !!!!!!!!!DA MODIFICARE!!!!!!!!!!!!
+
+var numUtente;
+var arrayUtente; 
+
+setTimeout(functionTimer, 5000);
+function functionTimer (){
+
+    alert('Sei pronto a giocare?');
+
+    for (var i = 0; i < 5; i++){
+
+        numUtente = parseInt(prompt('inserisci i numeri'));
+        arrayUtente = [];
+        
+        
+        
+    }
+    
+}
+
+
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
-
 
 
 
